8cbdc7c2e70dd75cc61ecee6379d2f42fa12ea32
KateFilatushkina <sitnikovakatarina@gmail.com>
2022-08-05 12:36:57 +0300

    Showing a current temperature
    Added temperature, city, description, humidity, wind

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "74b865ca5035f3429a2f339ef4d0bb28";
+let units = "metric";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
